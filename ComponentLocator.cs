a18743fac7f6a7440d9ce6074fc91268223c0303
TanTanDev <mrjonatanOlsson@gmail.com>
2019-02-20 17:03:04 +0100

    Add locator

---
+++ ComponentLocator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TanTanTools
+{
+    public class ComponentLocator : LocatorBase<Component>
+    {
+    }
+  
+}
---
+++ LocatorBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+// locate registered classes that might be referenced in another gameobject
+public class LocatorBase<T> : MonoBehaviour
+{
+    [SerializeField] protected T[] m_locations;
+
+    public A Locate<A>()
+        where A: class
+    {
+        T located = m_locations.First((check)=> { return (check.GetType() == typeof(A)); });
+        if (located == null)
+            return located as A;
+        return default;
+    }
+
+    public static A Locate<A>(GameObject a_go) where A: class
+    {
+        A located = null;
+        located = a_go.GetComponent<A>();
+        if (located != null)
+            return located;
+        LocatorBase<T> locator =  a_go.GetComponent<LocatorBase<T>>();
+        if (locator == null)
+            return located;
+        located = locator.Locate<A>();
+        return located;
+    }
+}
